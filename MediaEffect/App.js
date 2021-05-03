2aa4be26ab317e75622f98b0748959768bece1c5
DaVinciyS <83597450+DaVinciyS@users.noreply.github.com>
2021-05-03 19:47:28 +0300

    Add files via upload

---
+++ MediaEffect/App.js
@@ -0,0 +1,36 @@
+/*window.addEventListener("keydown", checkKeyPress, false);
+
+function checkKeyPress(key){
+  if(key.keyCode == "81"){
+    var element = document.getElementById("PBone_popup");
+    element.classList.toggle("PBone_popupAfter");
+  }
+
+}*/
+
+function MoreReadOne() {
+  var element = document.getElementById("PBone_popup");
+  element.classList.toggle("PBone_popupAfter");
+}
+function MoreReadThree() {
+  var element = document.getElementById("PBThird");
+  element.classList.toggle("PBThirdAfter");
+}
+function MoreReadEght(){
+  var element = document.getElementById("PBEght");
+  element.classList.toggle("PBEghtAfter");
+}
+function MoreReadTwnty(){
+  var element = document.getElementById("PBTwnty");
+  element.classList.toggle("PBTwntyAfter");
+}
+function MoreReadEghten(){
+  var element = document.getElementById("PBEghten");
+  element.classList.toggle("PBEghtenAfter");
+}
+function MoreReadSxten(){
+  var element = document.getElementById("PBSxten");
+  element.classList.toggle("PBSxtenAfter");
+}
+
+
